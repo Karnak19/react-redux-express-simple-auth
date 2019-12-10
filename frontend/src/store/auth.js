aace2f2f686e0a19704ad5933d1860964fef4884
Karnak19 <basile64.v@gmail.com>
2019-12-10 16:07:57 +0100

    import redux devtools only in dev env

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -28,7 +28,9 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === "development" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
